44a6e202c6d0b6282c64e2edc3c7288162b23345
burhan-source <78170030+burhan-source@users.noreply.github.com>
2021-05-11 23:50:31 +0500

    Delete Projects/projects-list/5. Quiz v3 directory

--- Projects/projects-list/5. Quiz v3/highlightText.js
+++
@@ -1,6 +0,0 @@
-const highlightText = document.querySelectorAll('.ask input');
-highlightText.forEach(option => {
-    var ok = option.addEventListener('click', e => {
-        console.log(e.toElement.nextElementSibling.style.color = 'white');
-    });
-});
